5f9015faffafa8378c5329c6e532df1919550a41
Bryan Yep <yepsan95@gmail.com>
2024-01-23 09:14:36 -0500

    feat: defines variables with type annotation.

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Defines and annotates the following variables with type annotations.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
