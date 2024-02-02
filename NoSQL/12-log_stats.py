99df658970465a69f8d4c01afdbb28722770d90e
Bryan Yep <yepsan95@gmail.com>
2024-02-02 13:49:48 -0500

    fix: optimized code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,25 +5,22 @@ from pymongo import MongoClient
 
 
 if __name__ == '__main__':
-    client = pymongo.MongoClient()
+    client = MongoClient()
     db = client.logs
     col = db.nginx
 
     number_of_logs = col.count_documents({})
-    number_of_get = col.count_documents({"method": {"$regex": "GET"}})
-    number_of_post = col.count_documents({"method": {"$regex": "POST"}})
-    number_of_put = col.count_documents({"method": {"$regex": "PUT"}})
-    number_of_patch = col.count_documents({"method": {"$regex": "PATCH"}})
-    number_of_delete = col.count_documents({"method": {"$regex": "DELETE"}})
+    print(f"{number_of_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print("Methods:")
+    for method in methods:
+        output = col.count_documents({"method": {"$regex": method}})
+        print(f"\tmethod {method}: {output}")
+
     status_check = col.count_documents({"path": "/status"})
 
-    print(f"{number_of_logs} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {number_of_get}")
-    print(f"\tmethod POST: {number_of_post}")
-    print(f"\tmethod PUT: {number_of_put}")
-    print(f"\tmethod PATCH: {number_of_patch}")
-    print(f"\tmethod DELETE: {number_of_delete}")
     print(f"{status_check} status check")
 
     client.close()
