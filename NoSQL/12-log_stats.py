ba9b5e0795fd143aa426eb70684195ba8db2e629
Bryan Yep <yepsan95@gmail.com>
2024-02-02 13:43:48 -0500

    fix: script now uses methods from pymongo library.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,34 +1,21 @@
 #!/usr/bin/env python3
 """Python script that provides some stats about Nginx logs stored in MongoDB.
 """
-import pymongo
+from pymongo import MongoClient
+
 
 if __name__ == '__main__':
     client = pymongo.MongoClient()
     db = client.logs
     col = db.nginx
-    cursor = col.find({})
-    number_of_logs = 0
-    number_of_get = 0
-    number_of_post = 0
-    number_of_put = 0
-    number_of_patch = 0
-    number_of_delete = 0
-    status_check = 0
-    for doc in cursor:
-        if doc['method'] == 'GET':
-            number_of_get += 1
-            if doc['path'] == '/status':
-                status_check += 1
-        elif doc['method'] == 'POST':
-            number_of_post += 1
-        elif doc['method'] == 'PUT':
-            number_of_put += 1
-        elif doc['method'] == 'PATCH':
-            number_of_patch += 1
-        elif doc['method'] == 'DELETE':
-            number_of_delete += 1
-        number_of_logs += 1
+
+    number_of_logs = col.count_documents({})
+    number_of_get = col.count_documents({"method": {"$regex": "GET"}})
+    number_of_post = col.count_documents({"method": {"$regex": "POST"}})
+    number_of_put = col.count_documents({"method": {"$regex": "PUT"}})
+    number_of_patch = col.count_documents({"method": {"$regex": "PATCH"}})
+    number_of_delete = col.count_documents({"method": {"$regex": "DELETE"}})
+    status_check = col.count_documents({"path": "/status"})
 
     print(f"{number_of_logs} logs")
     print("Methods:")
