db816b0b91d5a6960b24fa7305bb8e099398f186
Bryan Yep <yepsan95@gmail.com>
2024-02-01 23:32:16 -0500

    feat: Python function that changes all topics of a school document based on the name.

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines a function that changes all topics
+of a school document based on the name.
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates all topics of a school document"""
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
