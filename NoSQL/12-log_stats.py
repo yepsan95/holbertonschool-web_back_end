68172244e39fefd06f977068d9e97dd99fb89055
Bryan Yep <yepsan95@gmail.com>
2024-02-04 11:31:18 -0500

    fix: changed code structure.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,24 +4,25 @@
 from pymongo import MongoClient
 
 
-def helper(a: dict) -> int:
-    """Returns log"""
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
-    return logs.count_documents(a)
+def log_stats(col):
+    """Prints stats about Nginx logs stored in MongoDB"""
+    number_of_logs = col.count_documents({})
+    print(f"{number_of_logs} logs")
 
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-def main():
-    """Provides stats about Nginx logs stored in MongoDB"""
-    print(f"{helper({})} logs")
     print("Methods:")
-    print(f"\tmethod GET: {helper({'method': 'GET'})}")
-    print(f"\tmethod POST: {helper({'method': 'POST'})}")
-    print(f"\tmethod PUT: {helper({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {helper({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {helper({'method': 'DELETE'})}")
-    print(f"{helper({'method': 'GET', 'path': '/status'})} status check")
+    for method in methods:
+        output = col.count_documents({"method": method})
+        print(f"\tmethod {method}: {output}")
+
+    status_check = col.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    db = client.logs
+    col = db.nginx
+    log_stats(col)
+    client.close()
