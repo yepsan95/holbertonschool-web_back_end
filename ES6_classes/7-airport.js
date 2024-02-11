d4c9f98b2e1a63e7eecb0cde7606ac3b8484e608
Bryan Yep <yepsan95@gmail.com>
2024-02-11 14:33:32 -0800

    fix: eslint compliant

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -2,12 +2,12 @@ export default class Airport {
   constructor(name, code) {
     if (typeof (name) !== 'string') {
       throw TypeError('name must be a string');
-	}
+    }
     if (typeof (code) !== 'string') {
       throw TypeError('code must be a string');
-	}
+    }
     this._name = name;
-	this._code = code;
+    this._code = code;
   }
 
   toString() {
