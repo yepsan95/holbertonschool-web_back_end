7bb5937f5bb8dddab0e6abb40a437f454fdc2bb3
Bryan Yep <yepsan95@gmail.com>
2024-02-11 09:44:01 -0800

    fix: changed code structure

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
 import ClassRoom from './0-classroom.js';
 function initializeRooms() {
-  let classRoomsArray = [];
-  classRoomsArray[0] = new ClassRoom(19);
-  classRoomsArray[1] = new ClassRoom(20);
-  classRoomsArray[2] = new ClassRoom(34);
+  const classRoomsArray = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
   return classRoomsArray;
 }
