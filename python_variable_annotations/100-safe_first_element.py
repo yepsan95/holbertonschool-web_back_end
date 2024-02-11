f0d3eae0ac4c48826f2ed88a12a6f3f856e5b845
Bryan Yep <yepsan95@gmail.com>
2024-02-11 15:59:36 -0800

    fix: improved code

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Defines a type-annotated function.
 """
-from typing import Sequence, Any, Optional
+from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Type-annotated funciont that returns a list"""
     if lst:
         return lst[0]
