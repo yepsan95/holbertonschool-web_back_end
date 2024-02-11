9cd02df3b8827761c197f0445c4be954918109f5
Bryan Yep <yepsan95@gmail.com>
2024-02-11 13:55:31 -0800

    fix: eslint compliant

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -7,9 +7,7 @@ export default class Building {
       this.constructor !== Building
       && typeof (this.evacuationWarningMessage) !== 'function'
     ) {
-      throw Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
 
     this._sqft = sqft;
