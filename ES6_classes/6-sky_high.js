fcafc1d04a730f3aed833a4540e4957b1fd027ad
Bryan Yep <yepsan95@gmail.com>
2024-02-11 14:15:14 -0800

    feat: JavaScript script that implements a class named SkyHighBuilding that inherits from Building.

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,27 @@
+import Building from '5-building'
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof (sqft) !== 'number') {
+      throw TypeError('sqft must be a number');
+    }
+    if (typeof (floors) !== 'number') {
+      throw TypeError('floors must be a number');
+    }
+	super(sqft);
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    warningMessage = `Evacuate slowly the ${this.floors} floors`;
+	return warningMessage;
+  }
+
+  get sqft() {
+    return super._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+}
