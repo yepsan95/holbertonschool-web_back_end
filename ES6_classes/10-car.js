c655b452fa7ab313cf55e49df4fd0b3cd9c91eb5
Bryan Yep <yepsan95@gmail.com>
2024-02-11 15:18:13 -0800

    feat: JavaScript script that implements a class named Car.

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,21 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof (brand) !== 'string') {
+      throw TypeError('brand must be a string');
+    }
+    if (typeof (motor) !== 'string') {
+      throw TypeError('motor must be a string');
+    }
+    if (typeof (color) !== 'string') {
+      throw TypeError('color must be a string');
+    }
+    this._brand = brand;
+	this._motor = motor;
+	this._color = color;
+  }
+
+  cloneCar() {
+    let newCar = new Car(this._brand, this._motor, this._color);
+    return newCar;
+  }
+}
