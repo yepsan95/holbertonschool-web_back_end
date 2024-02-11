ff70a3d4f9bc9379f9b6783f25a6e36c14e81b45
Bryan Yep <yepsan95@gmail.com>
2024-02-11 14:48:31 -0800

    feat: JavaScript script that implements a class named HolbertonClass.

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,20 @@
+export default HolbertonClass {
+  constructor(size, location) {
+    if (typeof (size) !== 'number') {
+      throw TypeError('size must be a number');
+    }
+    if (typeof (location) !== 'string') {
+      throw TypeError('location must be a string');
+    }
+    this._size = size;
+	this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
