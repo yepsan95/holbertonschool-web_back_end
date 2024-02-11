82b7ae18123804bf348687e31f04d3e44f613212
Bryan Yep <yepsan95@gmail.com>
2024-02-11 09:56:04 -0800

    fix: JSlint compliant

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const classRoomsArray = [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
   return classRoomsArray;
 }
