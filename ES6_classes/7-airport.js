9fe59402f48fae1cc79e68ef8a37a8528b017a19
Bryan Yep <yepsan95@gmail.com>
2024-02-11 14:30:24 -0800

    feat: JavaScript script that implements a class named Airport.

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,16 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof (name) !== 'string') {
+      throw TypeError('name must be a string');
+	}
+    if (typeof (code) !== 'string') {
+      throw TypeError('code must be a string');
+	}
+    this._name = name;
+	this._code = code;
+  }
+
+  toString() {
+    return this._code;
+  }
+}
