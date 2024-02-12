e890d3ac21c667c8685a71ece1acd7cd415456c9
Bryan Yep <yepsan95@gmail.com>
2024-02-11 18:42:46 -0800

    fix: EVCar has teh correct definition.

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -2,9 +2,7 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color= color;
+    super(brand, motor, color);
     this._range = range;
   }
 
