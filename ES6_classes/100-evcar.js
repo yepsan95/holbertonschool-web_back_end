c12578324cd8805ceee120ce7c3d188c3adba80f
Bryan Yep <yepsan95@gmail.com>
2024-02-11 19:31:49 -0800

    fix: added comment for ESlint to ignore the rule 'class-methods-use-this'.

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -6,7 +6,8 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   cloneCar() {
-	  return new Car();
+    return new Car();
   }
 }
