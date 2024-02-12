fc867a5dbdb4e358c213b08b91dd26665cf726e6
Bryan Yep <yepsan95@gmail.com>
2024-02-11 18:40:59 -0800

    fix: improved code

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -2,7 +2,9 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, motor, color);
+    this._brand = brand;
+    this._motor = motor;
+    this._color= color;
     this._range = range;
   }
 
