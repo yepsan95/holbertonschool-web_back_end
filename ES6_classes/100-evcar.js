15c754f005cd968730f93e08d903b9f14a36043e
Bryan Yep <yepsan95@gmail.com>
2024-02-11 18:08:45 -0800

    fix: eslint compliant

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -2,7 +2,7 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-	super(brand, motor, color);
+    super(brand, motor, color);
     this._range = range;
   }
 
