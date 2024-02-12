86affc9fbd8a3c672ed7c1c4c419f3b0c68bd409
Bryan Yep <yepsan95@gmail.com>
2024-02-11 17:53:48 -0800

    fix: deleted unnecessary lines.

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,14 +1,5 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof (brand) !== 'string') {
-      throw TypeError('brand must be a string');
-    }
-    if (typeof (motor) !== 'string') {
-      throw TypeError('motor must be a string');
-    }
-    if (typeof (color) !== 'string') {
-      throw TypeError('color must be a string');
-    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
