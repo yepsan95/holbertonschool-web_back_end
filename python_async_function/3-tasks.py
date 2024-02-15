7913591dda502ad3977c7262456a7c68a0dee7bb
Bryan Yep <yepsan95@gmail.com>
2024-02-15 12:47:54 -0800

    feat: defined function task_wait_random that takes an integer max_delay and returns a asyncio.Task.

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defines a function task_wait_random that takes an integer
+max_delay and returns a asyncio.Task.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """Creates a task with the function wait_random and returns it
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
