49d4cd3483bca82825cfd1a5ee0a6aa6ea7399f6
Bryan Yep <yepsan95@gmail.com>
2024-02-15 18:37:43 -0800

    fix: pycode style

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Defines a coroutine called async_generator that takes no arguments.
 The coroutine will loop 10 times, each time asynchronously wait 1 second,
-then yield a random number between 0 and 10. 
+then yield a random number between 0 and 10.
 """
 import asyncio
 from typing import Generator
