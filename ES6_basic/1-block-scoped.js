ec7099450094eddcab322215feca86a307ebb00e
Bryan Yep <yepsan95@gmail.com>
2024-02-17 13:47:17 -0800

    fix: variables cannot be reassigned.

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,7 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) {}
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+	task = false;
+	task2 = true;
+  }
 
   return [task, task2];
 }
