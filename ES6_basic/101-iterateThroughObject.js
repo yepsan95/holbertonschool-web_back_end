3c4027aac30e3d25376e5b98c31c3e54905fd61e
Bryan Yep <yepsan95@gmail.com>
2024-02-18 11:54:46 -0800

    feat: JavaScriptscript that defines a function named iterateThroughObject.

---
+++ ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,10 @@
+export default function iterateThroughObject(reportWithIterator) {
+  result = `${reportWithIterator[0]}`;
+  if (reportWithIterator.length > 1) {
+    for (const employee of reportWithIterator.slice(1)) {
+      result = result + ` | ${employee}`;
+    }
+  }
+
+  return result;
+}
