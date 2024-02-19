8c8978b175f30ea420e320500c78d9eb5973580a
Bryan Yep <yepsan95@gmail.com>
2024-02-18 19:17:45 -0800

    fix: ESlint compliant.

--- ES6_basic/101-iterateThroughObject.js
+++ ES6_basic/101-iterateThroughObject.js
@@ -2,7 +2,7 @@ export default function iterateThroughObject(reportWithIterator) {
   let result = reportWithIterator[0];
   if (reportWithIterator.length > 1) {
     for (const employee of reportWithIterator.slice(1)) {
-      result = result + ' | ' + employee;
+      result = `${result} | ${employee}`;
     }
   }
   return result;
