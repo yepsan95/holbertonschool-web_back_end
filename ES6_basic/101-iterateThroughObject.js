bbc465b15206d745791d5b3c6f05e86ae6b9b29b
Bryan Yep <yepsan95@gmail.com>
2024-02-18 18:58:46 -0800

    fix: changed code structure.

--- ES6_basic/101-iterateThroughObject.js
+++ ES6_basic/101-iterateThroughObject.js
@@ -1,4 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const result = reportWithIterator.join(' | ');
+  const result = reportWithIterator[0]; 
+  if (reportWithIterator.length > 1) {
+    for (const employee of reportWithIterator.slice(1)) {
+      result = result + ' | ' + employee;
+    }
+  }
   return result;
 }
