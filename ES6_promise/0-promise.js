b28587e7ffb1b546760efffc78d60838926c5437
Bryan Yep <yepsan95@gmail.com>
2024-02-20 13:03:28 -0800

    fix: added reject statement to promise.

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
 export default function getResponseFromAPI() {
   const myPromise = new Promise((resolve, reject) => {
-    resolve("Success!");
+    resolve('Success!');
+    reject(new Error('Failure!'));
   });
   return myPromise;
 }
