d18c5f11d35f25967a9fc49b543467e79cf0e733
Bryan Yep <yepsan95@gmail.com>
2024-02-22 13:02:03 -0800

    fix: in catch: used console.log() instead of console.error().

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,9 +4,8 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const { body } = results[0];
-      const { firstName } = results[1];
-      const { lastName } = results[1];
+      const { firstName, lastName } = results[1];
       console.log(`${body} ${firstName} ${lastName}`);
     })
-    .catch(() => console.error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
