6994b55c0c2ae9c40a67c9bbe42454361f79e1d6
Bryan Yep <yepsan95@gmail.com>
2024-02-22 15:49:13 -0800

    fix: changed code structure.

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,10 @@
 export default function handleResponseFromAPI(promise) {
+  successObject = {
+    status: 200,
+    body: 'success',
+  };
   return promise
-    .then(() => ({
-      status: 200,
-	  body: 'success',
-    }))
-	.catch(() => Error())
-	.finally(() => console.warn('Got a response from the API'));
+    .then(() => successObject)
+	.catch(() => new Error())
+	.finally(() => console.log('Got a response from the API'));
 }
