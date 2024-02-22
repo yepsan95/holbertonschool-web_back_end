17e9dff502496e95bc72f7a9e81a251cbedfaf49
Bryan Yep <yepsan95@gmail.com>
2024-02-22 12:58:01 -0800

    feat: JavaScript function that returns a promise.

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const { body } = results[0];
+      const { firstName } = results[1];
+      const { lastName } = results[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.error('Signup system offline'));
+}
