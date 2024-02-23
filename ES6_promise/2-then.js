9893f45d924159082a070e48ac59456749a47bad
Bryan Yep <yepsan95@gmail.com>
2024-02-22 17:20:08 -0800

    fix: ESLint compliant.

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  const successObject = {
-    status: 200,
-    body: 'success',
-  };
   return promise
     .then(() => {
-      return successObject;
-    }
-	.catch(() => new Error())
-	.finally(() => console.log('Got a response from the API'));
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => new Error());
 }
