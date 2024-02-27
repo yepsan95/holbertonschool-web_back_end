8605600a2eb849ceffd20583b4aa2ffa1411948f
Bryan Yep <yepsan95@gmail.com>
2024-02-26 17:42:41 -0800

    fix: function named createInt8TypedArray that returns a new ArrayBuffer with an Int8 value at a specific position.

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
-  int8Array[position] = value;
-  return buffer;
+  const int8View = new Int8Array(buffer);
+  int8View[position] = value;
+  return int8View;
 }
